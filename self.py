908865339ea9914f5ead3734b15afb9a42e68853
Die, Master Monkey <lofimonkey@gmail.com>
2025-11-30 14:11:30 -0800

    Create self.py

---
+++ self.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+# Procgen output based on runtime case usage
+# unhacker@gmail.com
+
+# python3 self.py
+# 36
+# python3 self.py
+# 36
+# python3 SELF.py
+# 47
+# python3 self.PY
+# 38
+
+from random import*; seed(__file__); print(randint(0,64))
